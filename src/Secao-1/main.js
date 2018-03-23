ee46e79ee912042c69d7d1556bd5513d29ee1a16
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2018-03-23 00:45:07 -0300

    uso do ESLint

---
+++ src/Secao-1/main.js
@@ -0,0 +1,17 @@
+/*
+ * Arquivo: main.js
+ * Data: 23/03/2018
+ * Descrição: Arquivo responsável para tratar sobre o uso do ESLint
+ * Author: Glaucia Lemos
+ *
+ */
+
+const a = 3;
+const b = 2;
+const c = 'dha';
+
+function sum() {
+  return a + b + c;
+}
+
+sum();
