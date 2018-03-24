969be8a941ff3ccf177fb681c8aff3b55d81b5a1
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2018-03-24 01:28:38 -0300

    alteração no arquivo .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,12 @@
 module.exports = {
-    "extends": "airbnb-base"
+    "extends": "airbnb-base",
+    "plugins": [
+      "import"
+    ],
+    "rules": {
+      "no-console": "off",
+      "import/newline-after-import": "off",
+      "no-unused-vars": 0,
+      "max-len": 0
+    }
 };
