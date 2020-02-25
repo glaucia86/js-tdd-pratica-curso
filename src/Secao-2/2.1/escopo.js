b406c528db8c1dbf8b31ba11fc32b82fb40f9e3b
glaucia86 <glaucia_lemos86@hotmail.com>
2020-02-24 21:47:52 -0300

    Atualizando a estrutura dos arquivos

---
+++ src/Secao-2/2.1/escopo.js
@@ -0,0 +1,16 @@
+/**
+ * File: escopo.js
+ * Date: 28/02/2020
+ * Description:
+ * Author: Glaucia Lemos
+ */
+
+let animal;
+
+function sayAnimal(params) {
+  animal = 'gato';
+  console.log(animal);
+}
+
+sayAnimal();
+console.log(animal);
