3e868f969e809acb8d9b8fbe4f9b9ad601004bb4
glaucia86 <glaucia_lemos86@hotmail.com>
2020-02-25 21:53:07 -0300

    Atualizando a estrutura dos arquivos

---
+++ src/Secao-2/2.2/let.js
@@ -0,0 +1,17 @@
+/**
+ * File: let.js
+ * Date: 28/02/2020
+ * Description:
+ * Author: Glaucia Lemos
+ */
+
+var animal = 'cat';
+
+console.log(animal);
+
+{
+  let animal = 'dog';
+  console.log(animal);
+}
+
+console.log(animal);
