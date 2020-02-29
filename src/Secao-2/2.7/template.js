12357adfddde632a559ebc1e860c3cd2745bb7ba
glaucia86 <glaucia_lemos86@hotmail.com>
2020-02-29 00:19:39 -0300

    Realização do exercício 2.7

---
+++ src/Secao-2/2.7/template.js
@@ -0,0 +1,22 @@
+/**
+ * File: template.js
+ * Date: 28/02/2020
+ * Description:
+ * Author: Glaucia Lemos
+ */
+
+const lugar = {
+  moro: 'Rio de Janeiro',
+  amo: 'Tailândia',
+};
+
+const texto = `Eu moro no ${lugar.moro}. Mas, sou apaixonada pela ${lugar.amo}!`;
+
+console.log(texto);
+
+const frutas =
+`banana
+laranja
+maçã`;
+
+console.log(frutas);
