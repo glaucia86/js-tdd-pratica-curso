e8811b22764e3c233bd1ab157f1df38f5b39a3b1
glaucia86 <glaucia_lemos86@hotmail.com>
2020-02-28 22:07:16 -0300

    Reestruturando os arquivos.

---
+++ src/Secao-2/2.5/arrow.js
@@ -0,0 +1,28 @@
+/**
+ * File: arrow.js
+ * Date: 28/02/2020
+ * Description:
+ * Author: Glaucia Lemos
+ */
+
+const estados = ['Rio de Janeiro', 'São Paulo', 'Minas Gerais'];
+
+const melhoresCidades = estados.map(function (nome) {
+  return `Amamos ${nome}`;
+});
+
+console.log(melhoresCidades);
+
+// Com Arrow Function
+const cidades = ['Rio de Janeiro', 'São Paulo', 'Belo Horizonte'];
+
+const melhoresCidades2 = cidades.map(nome => `Amamos ${nome}`);
+
+console.log(melhoresCidades2);
+
+// Uso do Arrow Function Encadeado com 'filter' e 'map':
+const encadeado = cidades
+  .filter(nome => nome === 'São Paulo')
+  .map(nome => `Amamos ${nome}`);
+
+console.log(encadeado);
