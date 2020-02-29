a9525c3d875d11c8f72000b9356946c58f308b3a
glaucia86 <glaucia_lemos86@hotmail.com>
2020-02-28 22:00:28 -0300

    Reestruturando os arquivos.

---
+++ src/Secao-2/2.4/temporal.js
@@ -0,0 +1,10 @@
+/**
+ * File: temporal.js
+ * Date: 28/02/2020
+ * Description:
+ * Author: Glaucia Lemos
+ */
+
+// console.log(cat); // vai indicar erro porque a saída está sendo chamada primeiro
+let cat = 'miau';
+console.log(cat);
